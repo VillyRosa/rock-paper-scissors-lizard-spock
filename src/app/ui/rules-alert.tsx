6783fc1cf6d3a7fbc6fcbacf0453d2796c8beee2
VillyRosa <villyrosa@hotmail.com>
2024-01-09 18:01:15 -0300

    update

--- src/app/ui/rules-alert.tsx
+++ src/app/ui/rules-alert.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+
 export default function RulesAlert() {
     return (
-        <div className="absolute z-50 w-full h-full bg-black opacity-50">
-
+        <div className="absolute z-10 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+            <div className='p-4 bg-white rounded-md'>
+                <div className='w-full flex justify-between items-center'>
+                    <h1 className='text-blue-800 font-black'>RULES</h1>
+                    <button>
+                        <Image
+                        src='/images/icon-close.svg'
+                        width={20}
+                        height={20}
+                        alt='closeBtn'
+                        />
+                    </button>
+                </div>
+                <Image
+                src='/images/image-rules-bonus.svg'
+                width={400}
+                height={400}
+                alt='rules'
+                />
+            </div>
         </div>
     )
 }
